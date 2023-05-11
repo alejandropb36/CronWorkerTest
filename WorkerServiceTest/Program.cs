9edb87eef07041c0e7578dd87756732e0a53438a
Alejandro Ponce <alexponce36@hotmail.com>
2023-05-11 10:26:52 -0600

    feat: add woker test project

---
+++ WorkerServiceTest/Program.cs
@@ -0,0 +1,10 @@
+using WorkerServiceTest;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
